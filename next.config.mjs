9881c86be7b97666bcc79631695622ad8a07da9f
Sunil K. Nagar <31478521+skrnagar@users.noreply.github.com>
2025-03-08 22:07:18 +0000

    Restored to 'd9cd9e14b235db7cbe2c16d40b94a9711a44f08d'
    Replit-Restored-To: d9cd9e14b235db7cbe2c16d40b94a9711a44f08d

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,49 @@
+let userConfig = undefined
+try {
+  userConfig = await import('./v0-user-next.config')
+} catch (e) {
+  // ignore error
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  experimental: {
+    instrumentationHook: true,
+  },
   images: {
-    domains: [
-      'lephbkawjuyyygguxqio.supabase.co',
-      'ui-avatars.com',
-      'api.dicebear.com',
-      'placehold.co'
+    domains: ['lephbkawjuyyygguxqio.supabase.co'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lephbkawjuyyygguxqio.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
     ],
   },
-};
+}
 
-export default nextConfig;
+mergeConfig(nextConfig, userConfig)
+
+function mergeConfig(nextConfig, userConfig) {
+  if (!userConfig) {
+    return
+  }
+
+  for (const key in userConfig) {
+    if (
+      typeof nextConfig[key] === 'object' &&
+      !Array.isArray(nextConfig[key])
+    ) {
+      nextConfig[key] = {
+        ...nextConfig[key],
+        ...userConfig[key],
+      }
+    } else {
+      nextConfig[key] = userConfig[key]
+    }
+  }
+}
+
+export default nextConfig
