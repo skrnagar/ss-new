fa3b9c69c9e7dc8fdf2ce08962d6d2c66601a65e
Sunil K. Nagar <31478521+skrnagar@users.noreply.github.com>
2025-03-09 01:07:40 +0000

    Restored to 'f12de0a645fea4c285a3740b744d9114ee4e3fe0'
    Replit-Restored-To: f12de0a645fea4c285a3740b744d9114ee4e3fe0

--- next.config.mjs
+++ next.config.mjs
@@ -8,10 +8,8 @@ const nextConfig = {
     domains: [
       'lephbkawjuyyygguxqio.supabase.co', // Supabase storage
       'avatars.githubusercontent.com',
-      'lh3.googleusercontent.com',
-      '3adfc305-31eb-4741-9506-4d4b5862f73b-00-1phvtd2v60o1t.pike.replit.dev' // Replit domain
+      'lh3.googleusercontent.com'
     ],
-    unoptimized: process.env.NODE_ENV !== 'production',
   },
 };
 
